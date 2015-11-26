009f288b0890c675ca50a6f23a187e9b9e59b553
emilyh <emily.lhumble@gmail.com>
2015-11-26 14:49:01 +0100

    edit list files

--- rad_tag_depth.R
+++ rad_tag_depth.R
@@ -1,5 +1,7 @@
 # read process_radtag log files and determine depth of coverage per individual
 
+require(dplyr)
+
 temp_logs <-list.files(pattern="*.log")
 
 log2csv <- function(file){
@@ -15,14 +17,14 @@ lapply(1:length(logs), function(i) write.table(logs[[i]],
                                                 file = paste("temp/", temp_logs[i],".csv", sep = ""),
                                                 sep = "\t", row.names = F, col.names = F, quote = F))
 
-temp_csvs <- list.files(pattern="*.log.csv")
+temp_csvs <- list.files(path = "temp", pattern="*.log.csv")
+temp_csvs <- lapply(temp_csvs, function(x) paste("temp/", x, sep=""))
 
 read.log <- function(file){
   read.csv(file, sep = "\t", header = T) %>%
     select(Barcode:Retained) %>%
     mutate(pool = paste(substitute(file)))
 }
-
 
 log <- do.call(rbind, lapply(temp_csvs, read.log)) %>%
   mutate(percent.retained = Retained/Total) %>%
